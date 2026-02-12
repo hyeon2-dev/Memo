9500d5709843d5f914fd08ff726eadf37ef86dc4
hyeon2-dev <ski069182@gmail.com>
2026-02-12 20:36:40 +0900

    커feat: 커밋메세지 깨짐현상 확인

--- src/main/java/com/example/memo/entity/Memo.java
+++ src/main/java/com/example/memo/entity/Memo.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Memo {
 
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
